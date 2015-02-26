23f9cacc58764a2fc804dce4af1e222f94bcba51
Angelica Barreda B. <anjelyk.b@gmail.com>
2015-02-25 20:05:07 -0600

    one

---
+++ holamundo.java
@@ -0,0 +1,9 @@
+package holamundo;
+
+public class holaMundo()
+{
+	public static void mai8n(String[] args) {
+		System.out.println("hola Javaficadas");
+	}
+}
+
---
+++ prueba.java
@@ -0,0 +1,11 @@
+public class prueba
+{
+ public static void main(String[] args)
+	{ 
+	 System.out.println("Hola Mundo de Java"); 
+
+	 	for (int i=0;i<=10;i++) { 
+	 		System.out.println("www.portaltutorialesfacil.blogspot.com:"+i); 
+	 	} 
+	} 
+ } 
